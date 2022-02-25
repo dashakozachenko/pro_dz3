12afcd74230fabb0a32c9d57613c3cdee0142b03
dashakozachenko <dkozacenko4@gmail.com>
2022-02-25 18:06:14 +0200

    Script

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+let admin, name;
+name = "Джон";
+admin = name;
+
+alert( admin );
+
+let ourPlanetName = "Земля";
+let currentUserName = "Джон";
+
+console.log(ourPlanetName);
+console.log(currentUserName);
+
+let number = 45;
+console.log(typeof number)
+let string = "45";
+console.log(typeof string)
